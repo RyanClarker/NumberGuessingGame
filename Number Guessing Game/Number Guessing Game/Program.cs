e8250adc42b42ecf080228449358bedbf6ded8b3
Youngs285 <nitronite500@gmail.com>
2014-03-05 10:09:29 -0500

    new

---
+++ Number Guessing Game/Number Guessing Game/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Number_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputHLC, PlyNum;
+            int PlyNum
+        }
+    }
+}
