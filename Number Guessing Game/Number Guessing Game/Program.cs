878c01d76c12476177f9d3f08e5050f321d153a1
Youngs285 <nitronite500@gmail.com>
2014-03-06 10:10:00 -0500

    New

--- Number Guessing Game/Number Guessing Game/Program.cs
+++ Number Guessing Game/Number Guessing Game/Program.cs
@@ -9,8 +9,38 @@ namespace Number_Guessing_Game
     {
         static void Main(string[] args)
         {
-            string InputHLC, PlyNum;
-            int PlyNum
+            string Input;
+            int guess, low = 0, high = 100;
+           // Random random = new Random();
+           // int randomnumber = random.Next(0, 100);
+
+
+
+            while (true)
+            {
+                guess = (high + low) / 2;
+                Console.WriteLine("Is your number {0}?", guess);
+                Console.WriteLine("Higher? Lower? Correct?");
+                Input = Console.ReadLine();
+
+                if (Input == "Lower" || Input == "L" || Input == "l")
+                {
+                    high = guess - 1;
+                    //Console.WriteLine("{0}", randomnumber);
+                }
+
+                if (Input == "Higher" || Input == "H" || Input == "h")
+                {
+                    low = guess + 1;
+                    //Console.WriteLine("{0}", randomnumber);
+                }
+
+                if (Input == "Correct" || Input == "C" || Input == "c")
+                {
+                    Console.WriteLine("Yay I won!");
+                    break;
+                }
+            }
         }
     }
 }
